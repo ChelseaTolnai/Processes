8e7dfa985df6c34e6026ade4c31899a4b9ba004c
ChelseaTolnai <ChelseaTolnai@gmail.com>
2019-06-04 18:24:48 -0400

    Completed ex1

--- ex1/ex1.c
+++ ex1/ex1.c
@@ -8,7 +8,39 @@
 
 int main(void)
 {
-    // Your code here
+    int x = 100;
+    printf("Before Fork - x: %d\n", x);
+    int rc = fork();
+    printf("After Fork - x: %d\n", x);
+    x += x;
+    printf("After Fork - x_double: %d\n", x);
+    if (rc < 0) {
+        fprintf(stderr, "fork failed\n");
+        exit(1);
+    } else if (rc == 0) {
+        printf("Child (pid: %d) - x: ", (int) getpid());
+        printf("%d\n", x);
+        x *= 2;
+        printf("Child (pid: %d) - x_times2: ", (int) getpid());
+        printf("%d\n", x);
+    } else {
+        printf("Parent (pid: %d) - x: ", (int) getpid());
+        printf("%d\n", x);
+        x /= 2;
+        printf("Parent (pid: %d) - x_divide2: ", (int) getpid());
+        printf("%d\n", x);
+    }
 
     return 0;
 }
+
+// RETURNS
+// Before Fork - x: 100
+// After Fork - x: 100
+// After Fork - x_double: 200
+// Parent (pid: 51646) - x: 200
+// Parent (pid: 51646) - x_divide2: 100
+// After Fork - x: 100
+// After Fork - x_double: 200
+// Child (pid: 51647) - x: 200
+// Child (pid: 51647) - x_times2: 400
